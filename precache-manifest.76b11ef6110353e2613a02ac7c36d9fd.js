905a4d6688b857dcf74993e01e1abc43a9fa4db4
Nicolas De Boose <nikoms@gmail.com>
2019-01-28 16:54:26 +0100

    1edd836

---
+++ precache-manifest.76b11ef6110353e2613a02ac7c36d9fd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "52dd7ddf56d095bce57c6a06ae827824",
+    "url": "/fonts/materialdesignicons-webfont.52dd7ddf.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bbc62d2451af81cd239b",
+    "url": "/js/chunk-vendors.c555dd44.js"
+  },
+  {
+    "revision": "0153e87a3ae38b8bd207",
+    "url": "/js/app.eaf8711c.js"
+  },
+  {
+    "revision": "be340591e69213a0e722",
+    "url": "/js/about.db11daa4.js"
+  },
+  {
+    "revision": "55163bb79d32643c7ad2f5ee77f8ef17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffde4ef9934e55b39043a79edc8d005f",
+    "url": "/img/materialdesignicons-webfont.ffde4ef9.svg"
+  },
+  {
+    "revision": "ee2bb9f3231fa8ee5caf00b7af3e8aa8",
+    "url": "/fonts/materialdesignicons-webfont.ee2bb9f3.eot"
+  },
+  {
+    "revision": "95b69736aec6b8dcee401694e1ce718e",
+    "url": "/fonts/materialdesignicons-webfont.95b69736.woff2"
+  },
+  {
+    "revision": "7118957e19d480e02b81d7a1c01a2ea8",
+    "url": "/fonts/materialdesignicons-webfont.7118957e.ttf"
+  },
+  {
+    "revision": "bbc62d2451af81cd239b",
+    "url": "/css/chunk-vendors.cd5bf13e.css"
+  },
+  {
+    "revision": "be340591e69213a0e722",
+    "url": "/css/about.195a31d1.css"
+  }
+];
