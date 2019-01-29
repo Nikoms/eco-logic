653634a0f08f1c8a320a833572c60818efb9c7fb
Nicolas De Boose <nikoms@gmail.com>
2019-01-29 21:44:39 +0100

    Remove link

--- frontend/src/components/Chart.vue
+++ frontend/src/components/Chart.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="small">
         <line-chart :chart-data="datacollection"></line-chart>
-        <button @click="fillData()">Randomize</button>
     </div>
 </template>
 
