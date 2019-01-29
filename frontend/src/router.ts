d9415879d8451e4f52514b08b2ddafecd5466b1c
Nicolas De Boose <nikoms@gmail.com>
2019-01-29 02:30:48 +0100

    Fix bottom-nav: The home was always selected

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Index from './views/Index.vue';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'index',
+      component: Index,
+    },
+    {
+      path: '/home',
       name: 'home',
       component: Home,
     },
---
+++ frontend/src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class Index extends Vue {
+    mounted() {
+      // Redirection pour la bottom-nav. Si une des rubriques pointe vers "/", il semblerait qu'elle soit toujours sélectionnée (dans la ui), même si on passe à une autre rubrique
+      this.$router.push('home');
+    }
+  }
+</script>
