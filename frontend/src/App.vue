794f93112500a77b4830ef1165947fdabd2c0017
Nicolas De Boose <nikoms@gmail.com>
2019-03-22 14:09:05 +0100

    Put travel first

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,6 +20,10 @@
                 <span>Home</span>
                 <v-icon>mdi-home</v-icon>
             </v-btn>
+            <v-btn color="teal" flat :to="{ name: 'traveling'}">
+                <span>Travel</span>
+                <v-icon>mdi-gas-station</v-icon>
+            </v-btn>
             <v-btn color="teal" flat :to="{ name: 'electricity'}">
                 <span>Electricity</span>
                 <v-icon>mdi-flash</v-icon>
@@ -27,10 +31,6 @@
             <v-btn color="teal" flat :to="{ name: 'water'}">
                 <span>Water</span>
                 <v-icon>mdi-water</v-icon>
-            </v-btn>
-            <v-btn color="teal" flat :to="{ name: 'traveling'}">
-                <span>Travel</span>
-                <v-icon>mdi-gas-station</v-icon>
             </v-btn>
         </v-bottom-nav>
     </v-app>
