e94a73d73bcd5349789b4e5f5756285a431ea635
Nicolas De Boose <nikoms@gmail.com>
2019-03-29 00:04:55 +0100

    ease readability

--- core/co2/src/co2Listeners.ts
+++ core/co2/src/co2Listeners.ts
@@ -3,13 +3,11 @@ import { TravelEvents } from '@eco/core-travel/src/event/TravelEvents';
 import { ElectricityEvents } from '@eco/core-electricity/src/event/ElectricityEvents';
 import { EventDispatcher } from '@eco/core-shared-kernel/src/event/EventDispatcher';
 
-export const initCo2Listeners = (eventDispatcher: EventDispatcher, addCarbon: AddCarbon) => {
-  eventDispatcher.addListener(
-    TravelEvents.odometerUpdated, (e) => addCarbon.execute(AddCarbonRequest.fromOdometerEvent(e)));
+export const initCo2Listeners = (eventDispatcher: EventDispatcher, addCarbonCommand: AddCarbon) => {
+  const on = eventDispatcher.addListener.bind(eventDispatcher);
+  const addCarbon = addCarbonCommand.execute.bind(addCarbonCommand);
 
-  eventDispatcher.addListener(
-    TravelEvents.planeTravelAdded, (e) => addCarbon.execute(AddCarbonRequest.fromNewPlaneTravelEvent(e)));
-
-  eventDispatcher.addListener(
-    ElectricityEvents.powerUpdated, (e) => addCarbon.execute(AddCarbonRequest.fromPowerEvent(e)));
+  on(TravelEvents.odometerUpdated, (e) => addCarbon(AddCarbonRequest.fromOdometerEvent(e)));
+  on(TravelEvents.planeTravelAdded, (e) => addCarbon(AddCarbonRequest.fromNewPlaneTravelEvent(e)));
+  on(ElectricityEvents.powerUpdated, (e) => addCarbon(AddCarbonRequest.fromPowerEvent(e)));
 };
