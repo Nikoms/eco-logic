e9120ce9123d491521b9bb8a6d7d59d6c31d60ff
Nicolas De Boose <nikoms@gmail.com>
2019-07-22 00:38:24 +0200

    Display list of flights

--- frontend/reactts/src/traveling/views/Home.tsx
+++ frontend/reactts/src/traveling/views/Home.tsx
@@ -55,6 +55,18 @@ export default class Home extends React.Component<HomeProps> {
 
       {this.state.cars.map((car) => <Card key={car.id}><CardHeader title={car.name}/></Card>)}
 
+      <Card>
+        <CardHeader
+          title={this.state.flightTitle}
+          titleTypographyProps={{ align: 'center' }}
+          subheaderTypographyProps={{ align: 'center' }}
+          action={<AddIcon onClick={() => this.props.travelingFactory.ui.showAddFlight()}/>}
+        />
+      </Card>
+
+      {this.state.flights.map((flight, i) => <Card key={'flight' + i}><CardHeader
+        title={flight.distance + ' - ' + flight.date}/></Card>)}
+
     </div>);
   }
 }
